5d8c65b21c23d4545c9b5c317ebe104ed9598464
idamakela <idamakela98@gmail.com>
2023-02-02 11:53:42 +0100

    added text content for articles

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
             <article className={`active ${getActiveClassName(activeArticle, 'First Article')}`} >
                 <h1>About Me</h1>
                 <p>
-                    What I have done so far...
+                    What I have done so far is to build websites with HTML, CSS, Vanilla JS, JQuery and now React. 
                 </p>
                 <img className='pic' src={pic} alt=''/>
             </article>
@@ -27,10 +27,10 @@ function App() {
             <article className={`active ${getActiveClassName(activeArticle, '2nd Article')}`} >
                 <h1>Idea</h1>
                 <p>
-                    My idea for the personal project... 
+                    My idea for the personal project is a random number generator, hopefully that lets the user chose different types of dices and roll them the amount of times the user need them to. I am going to call it D20.
                 </p>
                 <p>
-                    The motivation behind the project...
+                    The motivation behind the project is Dungeons and Dragons. I have already done a random spell generator for a past assignment and thought bulding a mobile first site could benefit players of the game. 
                 </p>
                 <img className='pic' src={pic} alt=''/>
             </article>
@@ -38,10 +38,10 @@ function App() {
             <article className={`active ${getActiveClassName(activeArticle, '2nd Article')}`} >
                 <h1>Idea</h1>
                 <p>
-                    The design style...
+                    The design style for the project should include colors taken from the franchise and be simple and easy to understand. Therefore, I chose <a href='https://dribbble.com/shots/17423785-Weather-Interface'>this</a> design from dribble. 
                 </p>
                 <p>
-                    The motivation behind the choice...
+                    The motivation behind the choice is the simplicity of the design and the use of of rounded courner/borders which I feel create a softness around the design. 
                 </p>
                 <img className='pic' src={pic} alt=''/>
             </article>
